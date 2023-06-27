437beb53a1f8117f9f6c82a7d319c8f22c145d0b
Kim-taeshin <69833105+Kim-taeshin@users.noreply.github.com>
2023-06-27 20:24:26 +0900

    코드블럭 코드 변경

--- components/CodeBlock/index.vue
+++ components/CodeBlock/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="code-wrap">
         <div>{{ myTitle }}</div>
-        <pre><code class="code-block">{{ myCode }}</code></pre>
+        <div ref="ref_code"><pre><code class="code-block">{{ myCode }}</code></pre></div>
     </div>
 </template>
 
@@ -18,17 +18,24 @@ export default defineComponent ({
 
     setup(props)
     {
+        const ref_code = ref(null);
+
         const myTitle = ref(props.fileName);
         const myCode = ref(props.code);
         return {
-            myCode, myTitle,
+            ref_code, myCode, myTitle
         }
     },
 
     mounted() {
-        document.querySelectorAll("code").forEach((el)=>{
+        // document.querySelectorAll("code").forEach((el)=>{
+        //     hljs.highlightElement(el);
+        // })
+
+        if(this.ref_code){
+            const el = this.ref_code as HTMLElement;
             hljs.highlightElement(el);
-        })
+        }
 
         // setTimeout(()=> {
 
