3388e11fc49f6d02a356117ab8fce1d6a32260ca
Kim-taeshin <69833105+Kim-taeshin@users.noreply.github.com>
2023-06-27 18:46:24 +0900

    잘못된 클래스명 수정

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="contents">
-            <div clasee="box">
+            <div class="box">
                 <div class="title">2D 그래픽 라이브러리로 PIXI.JS를 선택한 이유</div>
                 <div class="sentence">
                     <h1>about</h1>
