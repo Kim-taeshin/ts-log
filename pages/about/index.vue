089d33d644abe3d25acbe703c70048369a28a258
Kim-taeshin <69833105+Kim-taeshin@users.noreply.github.com>
2023-06-27 17:58:45 +0900

    스타일 적용

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
-        <div clasee="box">
-            <div class="sentence">
-                <h1>about</h1>
-                <h2>about</h2>
-                <p>about</p>
-                <CodeBlock />
+        <div class="contents">
+            <div clasee="box">
+                <div class="title">2D 그래픽 라이브러리로 PIXI.JS를 선택한 이유</div>
+                <div class="sentence">
+                    <h1>about</h1>
+                    <h2>about</h2>
+                    <p>about</p>
+                    <CodeBlock />
+                </div>
             </div>
         </div>
     </div>
