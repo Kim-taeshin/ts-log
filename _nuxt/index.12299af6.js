f5de34109be9b95c33181bc58bd523d04a19c516
Kim-taeshin <69833105+Kim-taeshin@users.noreply.github.com>
2023-06-27 02:39:06 +0000

    Deploying to gh-pages from @ Kim-taeshin/ts-log@6bfe78c3b4231bd8f61e4d8caa59af4e75573bf9 🚀

--- _nuxt/index.12299af6.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,l as t,k as o,m as c}from"./entry.a18b3c24.js";const n={},s=c("div",null,"about",-1),_=[s];function a(r,d){return o(),t("div",null,_)}const i=e(n,[["render",a]]);export{i as default};
