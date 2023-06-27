25b132f14e93dfc852aa4b93ca824476bac435e9
Kim-taeshin <69833105+Kim-taeshin@users.noreply.github.com>
2023-06-27 19:43:12 +0900

    퍼블릭 폴더로 폰트 이동

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  alias: {
+    assets: "<rootDir>/assets",
+  },
   css: ["~/assets/css/global.css", "~/assets/css/fonts.css"],
 });
