a3eb775018cca768008b719400e9968eb46af7a1
Kim-taeshin <69833105+Kim-taeshin@users.noreply.github.com>
2023-07-01 06:41:37 +0900

    hljsVuePlugin 설치

--- plugins/highlight.js
+++ plugins/highlight.js
@@ -1,3 +1,4 @@
+import hljsVuePlugin from "@highlightjs/vue-plugin";
 import hljs from "highlight.js/lib/core";
 import css from "highlight.js/lib/languages/css";
 import javascript from "highlight.js/lib/languages/javascript";
@@ -6,14 +7,7 @@ hljs.registerLanguage("javascript", javascript);
 hljs.registerLanguage("css", css);
 
 export default defineNuxtPlugin((nuxtApp) => {
-  app.directive("highlight", {
-    mounted(el) {
-      hljs.highlightBlock(el);
-    },
-    updated(el) {
-      hljs.highlightBlock(el);
-    },
-  });
+  nuxtApp.vueApp.use(hljsVuePlugin);
 });
 
 // export default (app) => {
