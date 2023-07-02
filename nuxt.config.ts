9ea861ebfec028a15d0e85396b2d748144d545ec
Kim-taeshin <69833105+Kim-taeshin@users.noreply.github.com>
2023-07-02 19:58:43 +0900

    commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,19 +9,20 @@ export default defineNuxtConfig({
     "~/assets/css/fonts.css",
     "~/assets/css/nord.css",
   ],
-  plugins: ["~/plugins/highlight.ts"],
+  //client
+  plugins: ["~/plugins/highlight.client.ts"],
 
   // app: {
   //   head: {
   //     link: [
   //       {
   //         rel: "stylesheet",
-  //         href: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/default.min.css",
+  //         href: "//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/default.min.css",
   //       },
   //     ],
   //     script: [
   //       {
-  //         src: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js",
+  //         src: "//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js",
   //       },
   //     ],
   //   },
