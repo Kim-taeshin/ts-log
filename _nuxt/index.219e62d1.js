b3a4809c2540baec47af95e5ce90fc9a777f092f
Kim-taeshin <69833105+Kim-taeshin@users.noreply.github.com>
2023-07-03 21:58:43 +0000

    Deploying to gh-pages from @ Kim-taeshin/ts-log@00690392e6c077d08762e2b8b50ff533329d92db 🚀

--- _nuxt/index.219e62d1.js
+++
@@ -1,4 +0,0 @@
-import{d as a,r as n,H as d,_,C as m,k as s,l as c,m as t,t as i,D as u}from"./entry.8f2210a2.js";const p=a({props:{fileName:{type:String,default:"Syntax"},code:{type:String,default:`function testFnc($num: number): void
-            {
-                const n: number = 100;
-            }`}},setup(e){const o=n(e.fileName);return{myCode:n(e.code),myTitle:o}},mounted(){setTimeout(()=>{document.querySelectorAll("pre code").forEach(e=>d.highlightElement(e))},1e3)}});const h={class:"code-wrap"},f={class:"code-block"};function g(e,o,r,y,v,S){const l=m("highlight");return s(),c("div",h,[t("div",null,i(e.myTitle),1),u((s(),c("div",null,[t("pre",null,[t("code",f,i(e.myCode),1)])])),[[l]])])}const $=_(p,[["render",g]]);export{$ as _};
